147f56c266b2641fd9da7e17be7329464e13ce71
Etienne Baque <etiennebaque@gmail.com>
2014-02-14 16:30:48 +0400

    Added update scripts

--- src/draftlawparser/Parser.java
+++ src/draftlawparser/Parser.java
@@ -43,24 +43,41 @@ public class Parser {
 
 	public static final String SPREADSHEET_URL = "*****";
 
-	public static final LinkedList<String> colNames = new LinkedList<String>(Arrays.asList("registrationDate","registrationNumber","draftLawType",
-			"primaryDraftLaw","draftLawTitle","initiator","author","description","firstCommitteeHearing","firstPlenaryHearing","secondCommitteeHearing",
-			"secondPlenaryHearing","thirdCommitteeHearing","thirdPlenaryHearing","lawNumber","initialLawNumber","numberDaysDiscussion"));
-	
+	public static final LinkedList<String> colNames = new LinkedList<String>(Arrays.asList("registrationDate","registrationNumber","draftLawType","draftLawTypeEn",
+			"primaryDraftLaw","draftLawTitle","draftLawTitleEn","initiator","initiatorEn","author","authorEn","description","descriptionEn",
+			"firstCommitteeHearing","firstCommitteeHearingEn","firstPlenaryHearing","firstPlenaryHearingEn","secondCommitteeHearing","secondCommitteeHearingEn",
+			"secondPlenaryHearing","secondPlenaryHearingEn","thirdCommitteeHearing","thirdCommitteeHearingEn","thirdPlenaryHearing","thirdPlenaryHearingEn",
+			"lawNumber","numberDaysDiscussion"));
+
 	public static final LinkedList<String> statusLabelEn = new LinkedList<String>(Arrays.asList("1st Committee","1st Plenary","2nd Committee","2nd Plenary","3rd Committee","3rd Plenary"));
-	
+
 	public static final LinkedList<String> statusLabelKa = new LinkedList<String>(Arrays.asList("პირველი კომიტეტი","პირველი პლენარული","მეორე კომიტეტი","მეორე პლენარული","მესამე კომიტეტი","მესამე პლენარული"));
 
 	public static void main(String[] args) {
-		
-		String outputPath = args[0];
 
-		SpreadsheetService service = new SpreadsheetService("tig-draftlaw-spreadsheet");
+		if (args != null && args.length == 2){
 
-		SpreadsheetEntry spreadsheet = getGoogleSpreadsheet(service);
+			String outputPath = args[0];
+			String lastScrapingDateStr  = args[1];
 
-		if (spreadsheet != null){
-			processSpreadsheet(spreadsheet, service, outputPath);
+			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+			try {
+				Date lastScrapingDate = sdf.parse(lastScrapingDateStr);
+				SpreadsheetService service = new SpreadsheetService("tig-draftlaw-spreadsheet");
+
+				SpreadsheetEntry spreadsheet = getGoogleSpreadsheet(service);
+
+				if (spreadsheet != null){
+					processSpreadsheet(spreadsheet, service, outputPath, lastScrapingDate);
+				}
+			} catch (ParseException e) {
+				System.out.println("ERROR: date provided has not the following format: DD-MM-YYYY");
+				e.printStackTrace();
+			}
+
+
+		}else{
+			System.out.println("ERROR: arguments not provided! Please provide 2 arguments : <output path> <date of the last scraping: MM-DD-YYYY>");
 		}
 
 	}
@@ -116,7 +133,7 @@ public class Parser {
 
 	}
 
-	private static void processSpreadsheet(SpreadsheetEntry spreadsheet, SpreadsheetService service, String outputPath){
+	private static void processSpreadsheet(SpreadsheetEntry spreadsheet, SpreadsheetService service, String outputPath, Date lastScrapingDate) throws ParseException{
 		// Make a request to the API to fetch information about all
 		// worksheets in the spreadsheet.
 		List<WorksheetEntry> worksheets;
@@ -147,115 +164,147 @@ public class Parser {
 
 				System.out.println(title);
 
-				// Print the fetched information to the screen for this worksheet.
-				URL listFeedUrl = worksheet.getListFeedUrl();
+				// We are currently only taking care of the 3rd spreadsheet (FALL 2013)
+				String lastSpreadsheetTitle = "FALL 2013";
+				if (title != null && title.contains(lastSpreadsheetTitle)){
 
-				ListFeed listFeed = service.getFeed(listFeedUrl, ListFeed.class);
+					// Print the fetched information to the screen for this worksheet.
+					URL listFeedUrl = worksheet.getListFeedUrl();
 
-				// Iterate through each row, printing its cell values.
-				int countrow=0;
-				
-				for (ListEntry row : listFeed.getEntries()) {
-					// Print the first column's cell value
-					// Iterate over the remaining columns, and print each cell value
-					if (countrow > 0){
-						Map<String,String> rowData = new HashMap<String,String>();
+					ListFeed listFeed = service.getFeed(listFeedUrl, ListFeed.class);
 
-						int countForColumn = 0;
-						for (String tag : row.getCustomElements().getTags()) {
-							rowData.put(colNames.get(countForColumn), row.getCustomElements().getValue(tag));
-							countForColumn++;
-						}
+					// Iterate through each row, printing its cell values.
+					int countrow=0;
 
-						// We need the data after the most recent published draft law (13.06.2013)
-						String regDate = rowData.get("registrationDate");
-						String mostRecent = "17.06.2013";
-						SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-						boolean shouldBeAdded = false;
-						try {
-							if (regDate != null){
-								Date lawDate = sdf.parse(regDate);
-								Date mostRecentDate = sdf.parse(mostRecent);
-								shouldBeAdded = mostRecentDate.before(lawDate);
+					for (ListEntry row : listFeed.getEntries()) {
+						// Print the first column's cell value
+						// Iterate over the remaining columns, and print each cell value
+						if (countrow > 0){
+							Map<String,String> rowData = new HashMap<String,String>();
+
+							int countForColumn = 0;
+							for (String tag : row.getCustomElements().getTags()) {
+								rowData.put(colNames.get(countForColumn), row.getCustomElements().getValue(tag));
+								countForColumn++;
 							}
-						} catch (ParseException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						
-						String registrationNumber = rowData.get("registrationNumber");
 
-						if (shouldBeAdded){
-							if (isPrimaryDraft(rowData)){
-								// We're dealing with the parent law, and its hearing related information
-								
-								// Formatting data here, for the SQL query.
-								String registrationNumberForQuery = parseDataForQuery(rowData.get("registrationNumber"),true);
-								String draftLawTitle = parseDataForQuery(rowData.get("draftLawTitle"),true);
-								String initiator = parseDataForQuery(rowData.get("initiator"),true);
-								String author = parseDataForQuery(rowData.get("author"),true);
-								String summary = parseDataForQuery(rowData.get("description"),false);
-								
-								String slug = registrationNumberForQuery.replaceAll("#", "").replaceAll("/", "");
-
-								String registrationDate = null;
-								if (rowData.get("registrationDate") != null){
-									registrationDate = "TO_DATE('"+rowData.get("registrationDate")+"','DD.MM.YYYY')";
-								}else{
-									registrationDate = "TO_DATE('01.01.1970','DD.MM.YYYY')";
+							// We need the data after the most recent published draft law (13.06.2013)
+							String regDate = rowData.get("registrationDate");
+							SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+							boolean shouldBeAdded = false;
+							try {
+								if (regDate != null){
+									Date lawDate = sdf.parse(regDate);
+									shouldBeAdded = lastScrapingDate.before(lawDate);
 								}
+							} catch (ParseException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 
-								// Definition of the draft law's current status
-								String statusEnglish = defineCurrentStatus(rowData,statusLabelEn);
+							String registrationNumber = rowData.get("registrationNumber");
+
+							// Formatting data here, for the SQL query.
+							String registrationNumberForQuery = parseDataForQuery(rowData.get("registrationNumber"),true);
+							String draftLawTitle = parseDataForQuery(rowData.get("draftLawTitle"),true);
+							String draftLawTitleEn = parseDataForQuery(rowData.get("draftLawTitleEn"),true);
+							String initiator = parseDataForQuery(rowData.get("initiator"),true);
+							String initiatorEn = parseDataForQuery(rowData.get("initiatorEn"),true);
+							String author = parseDataForQuery(rowData.get("author"),true);
+							String authorEn = parseDataForQuery(rowData.get("authorEn"),true);
+							String summary = parseDataForQuery(rowData.get("description"),false);
+							String summaryEn = parseDataForQuery(rowData.get("descriptionEn"),false);
+							
+							String registrationDate = null;
+							if (rowData.get("registrationDate") != null){
+								registrationDate = "TO_DATE('"+rowData.get("registrationDate")+"','DD.MM.YYYY')";
+							}else{
+								registrationDate = "TO_DATE('01.01.1970','DD.MM.YYYY')";
+							}
+							
+							// Definition of the draft law's current status
+							String statusEnglish = defineCurrentStatus(rowData,statusLabelEn);
+							String statusGeorgian = defineCurrentStatus(rowData,statusLabelKa);
+
+
+							if (isPrimaryDraft(rowData)){
+								if (shouldBeAdded){
+									// We're dealing with the parent law, and its hearing related information
+
+									String slug = registrationNumberForQuery.replaceAll("#", "").replaceAll("/", "");
+
+									
+
+									if (registrationNumberForQuery.trim().contains("110")){
+										System.out.println();
+									}
+
+									// Insert main information into draftlaw_draftlaw table
+									String insertQuery = "INSERT INTO draftlaw_draftlaw (bureau_date,bill_number,title,title_en,"
+											+ "title_ka,initiator,initiator_en,initiator_ka,author,author_en,author_ka,status,status_en,status_ka,"
+											+ "summary,summary_en,summary_ka,full_text,full_text_url,enacted_text_url,related_1,related_2,related_3," 
+											+ "related_4,related_5,slug) "
+											+ "VALUES ("+registrationDate+","+ registrationNumberForQuery.trim() +","+draftLawTitle+","+draftLawTitleEn+","
+											+draftLawTitle+","+initiator+","+initiatorEn+","+initiator+","+author+","+authorEn+","+author
+											+","+statusEnglish+","+statusEnglish+","+statusGeorgian+","+summary+","+summaryEn+","+summary+","
+											+ "'','','','','','','','',"+slug+");\n";
+
+									
+
+									bw.write(insertQuery);
+
+
+								}else{
+									// The parent law has already been added. We're doing updates here then. 
+									
+									String updateQuery = "UPDATE draftlaw_draftlaw SET "
+											+ "title_ka = " + draftLawTitle + ", "
+											+ "title_en = " + draftLawTitleEn + ", "
+											+ "initiator_ka = " + initiator + ", "
+											+ "initiator_en = " + initiatorEn + ", "
+											+ "author_ka = " + author + ", "
+											+ "author_en = " + authorEn + ", "
+											+ "status_ka = " + statusGeorgian + ", "
+											+ "status_en = " + statusEnglish + ", "
+											+ "summary_ka = " + summary + ", "
+											+ "summary_en = " + summaryEn + " "
+											+ "WHERE bureau_date = " + registrationDate + " AND bill_number = " + registrationNumberForQuery.trim() + " AND "
+											+ "title = " + draftLawTitle + ";\n";
+									
+									
+									bw.write(updateQuery);
+									
+								}
 								
-								String statusGeorgian = defineCurrentStatus(rowData,statusLabelKa);
-
-								// Insert main information into draftlaw_draftlaw table
-								String insertQuery = "INSERT INTO draftlaw_draftlaw (bureau_date,bill_number,title,title_en,"
-										+ "title_ka,initiator,initiator_en,initiator_ka,author,author_en,author_ka,status,status_en,status_ka,"
-										+ "summary,summary_en,summary_ka,full_text,full_text_url,enacted_text_url,related_1,related_2,related_3," 
-										+ "related_4,related_5,slug) "
-										+ "VALUES ("+registrationDate+","+ registrationNumberForQuery.trim() +","+draftLawTitle+","+draftLawTitle+","
-										+draftLawTitle+","+initiator+","+initiator+","+initiator+","+author+","+author+","+author
-										+","+statusEnglish+","+statusEnglish+","+statusGeorgian+","+summary+","+summary+","+summary+","
-										+ "'','','','','','','','',"+slug+");\n";
-
-								bw.write(insertQuery);
-
-								// Insert into draftlaw_draftlawdiscussion table the information related to hearing, for each draft law.
-								String insertHearingQuery = "INSERT INTO draftlaw_draftlawdiscussion (draftlaw_id,date,stage,place,place_en,place_ka,passed) VALUES ";
-
 								// Completing the insert queries with hearing info.
-								writeHearingQuery(insertHearingQuery, rowData.get("firstCommitteeHearing"), registrationNumber, bw, 0);
-								writeHearingQuery(insertHearingQuery, rowData.get("firstPlenaryHearing"), registrationNumber, bw, 1);
-								writeHearingQuery(insertHearingQuery, rowData.get("secondCommitteeHearing"), registrationNumber, bw, 2);
-								writeHearingQuery(insertHearingQuery, rowData.get("secondPlenaryHearing"), registrationNumber, bw, 3);
-								writeHearingQuery(insertHearingQuery, rowData.get("thirdCommitteeHearing"), registrationNumber, bw, 4);
-								writeHearingQuery(insertHearingQuery, rowData.get("thirdPlenaryHearing"), registrationNumber, bw, 5);
+								writeLawHearingQueries(bw, lastScrapingDate, rowData, registrationNumberForQuery);
 
 								bw.write("\n");
+								
+								
 							}else{
 								// We're dealing with a child law
-								String registrationNumberForQuery = parseDataForQuery(rowData.get("registrationNumber"),true);
 								registrationNumberForQuery = registrationNumberForQuery.trim();
 
 								if (!registrationNumberForQuery.isEmpty()){
 
+									String childLawTitleEn = parseDataForQuery(rowData.get("draftLawTitleEn"),true);
 									String childLawTitle = parseDataForQuery(rowData.get("draftLawTitle"),true);
 									String lawNumber = parseDataForQuery(rowData.get("lawNumber"),true);
 
 									String parentIdClause = "(SELECT id FROM draftlaw_draftlaw WHERE bill_number=" + registrationNumberForQuery +")";
 
 									String insertQuery = "INSERT INTO draftlaw_draftlawchild (parent_id,bill_number,title,law_number,title_en,title_ka,place) VALUES ("
-											+ parentIdClause + ","+registrationNumberForQuery+","+childLawTitle+","+lawNumber+","+childLawTitle+","+childLawTitle+",'');";
+											+ parentIdClause + ","+registrationNumberForQuery+","+childLawTitle+","+lawNumber+","+childLawTitleEn+","+childLawTitle+",'');";
 
-									childLawBw.write(insertQuery + "\n");
+									//childLawBw.write(insertQuery + "\n");
 								}
 							}
-						}
 
+
+						}
+						countrow++;
 					}
-					countrow++;
 				}
 			}
 			bw.close();
@@ -266,7 +315,7 @@ public class Parser {
 			files[0] = file;
 			files[1] = childLawFile;
 
-			File outputFile = new File(outputPath+"/insertDraftLaws.sql");
+			File outputFile = new File(outputPath+"/updateDraftLaws.sql");
 
 			if (!outputFile.exists()) {
 				outputFile.createNewFile();
@@ -281,9 +330,21 @@ public class Parser {
 			System.out.println("An error occured while instantiating SpreadsheedFeed.");
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Done.");
 
+	}
+
+		
+	private static void writeLawHearingQueries(BufferedWriter bw, Date lastScrapingDate, Map<String,String> rowData, String registrationNumber) throws IOException, ParseException{
+		
+		writeHearingQuery(lastScrapingDate, rowData.get("firstCommitteeHearing"), registrationNumber, bw, 0);
+		writeHearingQuery(lastScrapingDate, rowData.get("firstPlenaryHearing"), registrationNumber, bw, 1);
+		writeHearingQuery(lastScrapingDate, rowData.get("secondCommitteeHearing"), registrationNumber, bw, 2);
+		writeHearingQuery(lastScrapingDate, rowData.get("secondPlenaryHearing"), registrationNumber, bw, 3);
+		writeHearingQuery(lastScrapingDate, rowData.get("thirdCommitteeHearing"), registrationNumber, bw, 4);
+		writeHearingQuery(lastScrapingDate, rowData.get("thirdPlenaryHearing"), registrationNumber, bw, 5);
+		
 	}
 
 	/**
@@ -318,40 +379,61 @@ public class Parser {
 
 	/**
 	 * Completes SQL queries, when inserting data in draftlaw_draftlawdiscussion table
+	 * @param lastScrapingDate 
 	 * @param insertHearingQuery
 	 * @param hearingDescription
 	 * @param registrationNumber
 	 * @param bw
 	 * @param stage
 	 * @throws IOException
+	 * @throws ParseException 
 	 */
-	private static void writeHearingQuery(String insertHearingQuery, String hearingDescription, 
-			String registrationNumber, BufferedWriter bw, int stage) throws IOException{
+	private static void writeHearingQuery(Date lastScrapingDate, String hearingDescription, 
+			String registrationNumber, BufferedWriter bw, int stage) throws IOException, ParseException{
+		
+		
+		String insertQuery = "INSERT INTO draftlaw_draftlawdiscussion (draftlaw_id,date,stage,place,place_en,place_ka,passed) VALUES ";
+		String deleteQuery = "DELETE FROM draftlaw_draftlawdiscussion ";
+		
 		if (hearingDescription != null && !hearingDescription.isEmpty()){
 
 			String hearingValues = null;
-			String hearingDate = getDateFromHearingDescription(hearingDescription);
+			String hearingDateStr = getDateFromHearingDescription(hearingDescription);
 			String hearingDateForQuery = null;
-			if (hearingDate != null){
-				hearingDateForQuery = "TO_DATE('"+hearingDate+"','DD.MM.YYYY')";
-			}else{
-				hearingDateForQuery = "TO_DATE('01.01.1970','DD.MM.YYYY')";
-			}
-
-			String place = null;
-			if (stage % 2 == 0){
-				place = "Committee";
-			}else{
-				place = "Plenary";
-			}
-			if (registrationNumber != null){
-				hearingValues = ("((SELECT id FROM draftlaw_draftlaw WHERE bill_number = '" + registrationNumber.trim() +"'),"+hearingDateForQuery+","+Integer.toString(stage)+
-						",'"+place+"','"+place+"','"+place+"','N');");
+			SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+			Boolean isHearingInformationAleardyAdded = false;
+			if (hearingDateStr != null){
+				hearingDateForQuery = "TO_DATE('"+hearingDateStr+"','DD.MM.YYYY')";
+				Date hearingDate = sdf.parse(hearingDateStr);
+				isHearingInformationAleardyAdded = lastScrapingDate.after(hearingDate);
 				
+			}else{
+				// There is no way to track if a law discussion that does not have any date has been already added. So we delete the releant record,
+				// before adding it again.
+				hearingDateForQuery = "TO_DATE('01.01.1970','DD.MM.YYYY')";
+				String deleteWhere = "WHERE draftlaw_id = (SELECT id FROM draftlaw_draftlaw WHERE bill_number = " + registrationNumber.trim() + ") AND stage = " + Integer.toString(stage) + ";";
+				bw.write(deleteQuery + deleteWhere + "\n");
 			}
-
-			if (hearingValues != null){
-				bw.write(insertHearingQuery + hearingValues+"\n");
+			
+			if (!isHearingInformationAleardyAdded){
+				String place = null;
+				String placeKa = null;
+				if (stage % 2 == 0){
+					place = "Committee";
+					placeKa = "კომიტეტი";
+				}else{
+					place = "Plenary";
+					placeKa = "პლენარული";
+				}
+				if (registrationNumber != null){
+					hearingValues = ("((SELECT id FROM draftlaw_draftlaw WHERE bill_number = " + registrationNumber.trim() +"),"+hearingDateForQuery+","+Integer.toString(stage)+
+							",'"+place+"','"+place+"','"+placeKa+"','N');");
+	
+				}
+	
+				if (hearingValues != null){
+					bw.write(insertQuery + hearingValues+"\n");
+				}
 			}
 		}
 	}
